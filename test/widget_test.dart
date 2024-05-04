157526fe4349d75f63a81586d9f5a7ac3261058b
Nada Rizk <63368547+Nada-gaber@users.noreply.github.com>
2024-05-04 14:16:43 +0200

    shop cart changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:shopping_list/main.dart';
+import 'package:shopping_list/shopping/shop_cart.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ShoppingList());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
